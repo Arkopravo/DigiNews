8d9df0acb8fecd30f395a3dd55f1ecdaf62b6a86
Arkopravo Ghosh <arkopravoghosh789.com>
2024-11-27 10:26:56 +0530

    singlepost item component

--- src/app/postitems/[id]/page.tsx
+++ src/app/postitems/[id]/page.tsx
@@ -4,12 +4,30 @@ import React, { useEffect, useState } from 'react';
 import {initialPost} from "@/sections/Posts";
 import "./style.css"
 import Image from 'next/image';
+import Preloader from '@/components/Preloader';
+import { tabsData } from '@/data/data';
+import SidePostItem from '@/components/SidePostItem';
+import { PostProps } from '@/sections/Posts';
 
 // for single item
 const PostItem = ({params}: {params: {id: string}}) => {
 
     const id: string = params.id;
     const [item, setItem] = useState(initialPost);
+    const [items, setItems] = useState([]);
+
+
+    const [tabs, setTabs] = useState(tabsData);
+
+    const handleTabActive = (id: number): void => {
+        setTabs(tabsData.map(tab => {
+            tab.active = false;
+            if(tab.id === id) {
+                tab.active = true;
+            }
+            return tab;
+        }))
+    };
 
     const getSinglePostData = () => {
         fetch(`/api/postitems/${id}`)
@@ -18,8 +36,16 @@ const PostItem = ({params}: {params: {id: string}}) => {
         .catch(e => console.log(e.message))
     }
 
+    const getItemsData = () => {
+        fetch(`/api/postitems`)
+          .then((res) => res.json())
+          .then((data) => setItems(data))
+          .catch((e) => console.log(e.message));
+      };
+
     useEffect(() => {
         getSinglePostData();
+        getItemsData();
     }, []);
 
   return (
@@ -28,7 +54,8 @@ const PostItem = ({params}: {params: {id: string}}) => {
             <div className="container">
                 <div className="row">
                     <div className="col-md-9 post-content">
-                        <div className="single-post">
+                        {item && item.category !== '' ? (
+                            <div className="single-post">
                             <div className="post-meta">
                                 <span className="date">{item.category}</span>
                                 <span className="mx-1">
@@ -51,7 +78,8 @@ const PostItem = ({params}: {params: {id: string}}) => {
                             </p>
 
                             <figure className='my-4'>
-                                <Image src={`/${item.img}`} alt="" width={100} height={100} className='img-fluid' layout='responsive'/>
+                                {/* <Image src={`/${item.img}`} alt="" width={100} height={100} className='img-fluid' layout='responsive'/> */}
+                                <img src={`/${item.img}`} alt="" className='img-fluid'/>
                                 <figcaption>
                                     Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatum, est illum veritatis hic dicta, ipsam ipsum nisi doloremque
                                 </figcaption>
@@ -68,6 +96,55 @@ const PostItem = ({params}: {params: {id: string}}) => {
 
                             <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Molestias ratione culpa, distinctio, incidunt eaque inventore perferendis doloremque, sequi hic maxime fugit beatae ullam! Sed quia mollitia fugiat accusantium doloremque totam facere, nostrum exercitationem, corporis iste inventore quos rem tenetur placeat reiciendis itaque, natus est libero? Fuga excepturi modi nulla quae.
                             </p>
+                        </div>
+                        ) : (
+                            <Preloader/>
+                        )}
+                    </div>
+
+                    <div className='col-md-3'>
+                        <div className="aside-block">
+                            <ul className='nav nav-pills custom-tab-nav mb-4'>
+                                {tabs.map((tab) => (
+                                        <li className='nav-item' key={tab.id}>
+                                            <button 
+                                                className={`nav-link ${tab.active ? "active" : undefined}`}
+                                                onClick={() => handleTabActive(tab.id)}
+                                            >
+                                                {tab.name}
+                                            </button>
+                                        </li>
+                                    ))
+                                }
+                            </ul>
+
+                            <div className="tab-content">
+                                <div className={`tab-pane fade ${tabs[0].active ? 'show active' : ''}`}>
+                                    {
+                                    items.slice(0, 6)
+                                    .map((item: PostProps['item']) => (
+                                        <SidePostItem key={item._id} item={item} />
+                                    ))}
+                                </div>
+
+                                <div className={`tab-pane fade ${tabs[1].active ? 'show active' : ''}`}>
+                                    {
+                                    items.slice(6, 12)
+                                    .map((item: PostProps['item']) => (
+                                        <SidePostItem key={item._id} item={item} />
+                                    ))}
+                                </div>
+                            </div>
+                        </div>
+
+                        <div className="aside-vlock">
+                            <h3 className='aside-title'>Video</h3>
+                            <div className="video-post">
+                                <a href="https://www.youtube.com/shorts/Hp4TJlAywrI?feature=share" target='_blank' className='link-video'>
+                                    <span className="bi-play-fill"></span>
+                                    <img src='/assets/img/post-landscape-3.jpg' alt='' className='img-fluid' />
+                                </a>
+                            </div>
                         </div>
                     </div>
                 </div>
--- src/sections/Posts.tsx
+++ src/sections/Posts.tsx
@@ -7,7 +7,6 @@ import PostItemOne from "@/components/PostItemOne";
 import TrendingPost from "@/components/TrendingPost";
 import Preloader from "@/components/Preloader";
 
-
 export interface PostProps {
   item: {
     _id: string;
@@ -22,22 +21,20 @@ export interface PostProps {
 }
 
 export const initialPost = {
-  _id: '',
-  img: '',
-  category: '',
-  date: '',
-  title: '',
-  brief: '',
-  avatar: '',
-  author: '',
-}
-
+  _id: "",
+  img: "",
+  category: "",
+  date: "",
+  title: "",
+  brief: "",
+  avatar: "",
+  author: "",
+};
 
 export default function Posts() {
   const router = useRouter();
   const [items, setItems] = useState([]);
-  const[item, setItem] = useState(initialPost);
-
+  const [item, setItem] = useState(initialPost);
 
   const getItemsData = () => {
     fetch(`/api/postitems`)
@@ -46,22 +43,21 @@ export default function Posts() {
       .catch((e) => console.log(e.message));
   };
 
-
   const getSinglePostData = (id: string) => {
     fetch(`/api/postitems/${id}`)
-    .then(res => {
-      if(res.status === 404) {
-        router.push('/not-found')
-      }
-      return res.json();
-    })
-    .then(data => setItem(data))
-    .catch(error => console.log(error.message));
-  }
+      .then((res) => {
+        if (res.status === 404) {
+          router.push("/not-found");
+        }
+        return res.json();
+      })
+      .then((data) => setItem(data))
+      .catch((error) => console.log(error.message));
+  };
 
   useEffect(() => {
     getItemsData();
-    getSinglePostData('673f2cc6b6559a0ad3ab8897');
+    getSinglePostData("673f2cc6b6559a0ad3ab8897");
   }, []);
 
   // console.log(items);
@@ -70,60 +66,61 @@ export default function Posts() {
     <section id="posts" className="posts">
       <div className="container" data-aos="fade-up">
         <div className="row g-5">
-
           <div className="col-lg-4">
             <PostItemOne large={true} item={item} />
           </div>
 
           <div className="col-lg-8">
             <div className="row g-5">
-
               <div className="col-lg-4 border-start custom-border">
-                {items &&
-                  items.length > 0 ? (items.filter((item: {trending: boolean; top: boolean}) => 
-                    !item.trending && !item.top
-                  ).slice(0, 3)
-                  .map(
-                    (item: PostProps) => (
-                      <PostItemOne key={item._id} large={false} item={item} />
+                {items && items.length > 0 ? (
+                  items
+                    .filter(
+                      (item: { trending: boolean; top: boolean }) =>
+                        !item.trending && !item.top
                     )
-                  )) : (
-                    <Preloader/>
-                  )
-                  }
+                    .slice(0, 3)
+                    .map((item: PostProps['item']) => (
+                      <PostItemOne key={item._id} large={false} item={item} />
+                    ))
+                ) : (
+                  <Preloader />
+                )}
               </div>
 
               <div className="col-lg-4 border-start custom-border">
-                {items &&
-                  items.length > 0 ? (
-                    items.filter((item: {trending: boolean; top: boolean}) => 
-                      !item.trending && !item.top
-                    ).slice(3, 6)
-                    .map(
-                      (item: PostProps) => (
-                        <PostItemOne key={item._id} large={false} item={item} />
-                      )
+                {items && items.length > 0 ? (
+                  items
+                    .filter(
+                      (item: { trending: boolean; top: boolean }) =>
+                        !item.trending && !item.top
                     )
-                  ) : (
-                    <Preloader/>
-                  )
-                }
+                    .slice(3, 6)
+                    .map((item: PostProps['item']) => (
+                      <PostItemOne key={item._id} large={false} item={item} />
+                    ))
+                ) : (
+                  <Preloader />
+                )}
               </div>
 
               <div className="col-lg-4">
                 <div className="trending">
                   <h3>Trending</h3>
                   <ul className="trending-post">
-                    {
-                      items && items.length>0 ? (
-                        items.filter((item: {trending: boolean}) => item.trending)
-                        .map((item: PostProps, index: number) => (
-                          <TrendingPost key={item._id} item={item} index={index} />
+                    {items && items.length > 0 ? (
+                      items
+                        .filter((item: { trending: boolean }) => item.trending)
+                        .map((item: PostProps['item'], index: number) => (
+                          <TrendingPost
+                            key={item._id}
+                            item={item}
+                            index={index}
+                          />
                         ))
-                        ) : (
-                          <Preloader/>
-                        )
-                    }
+                    ) : (
+                      <Preloader />
+                    )}
                   </ul>
                 </div>
               </div>
